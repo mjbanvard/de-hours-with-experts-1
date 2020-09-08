8d269c898acc7f6c5b170c8b93d236de8a7f0375
Michael Banvard <mjbanvard@gmail.com>
2020-09-08 15:49:57 -0500

    150 out of 150 passed.
    Learned about breaking out of nested loop. Pushed loop to go full length of array.
    
    Pleased as punch!

--- java/src/main/java/com/labs1904/NextBiggestNumber.java
+++ java/src/main/java/com/labs1904/NextBiggestNumber.java
@@ -1,9 +1,6 @@
 package com.labs1904;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class NextBiggestNumber {
 
@@ -29,18 +26,18 @@ public class NextBiggestNumber {
         for (int i = 0; i < numElements; i++) {
             buildArray[i] = iString.charAt( i ) - '0';
         }
-        int[] origBuild = buildArray;
 
         // Now that the array is created, we need to make a method that juggles the indeces, eliminates the
         // values less than the original number, and narrows down the remaining numbers to find the one that is
         // "just greater than" the given input.
 
-        for (int i = 2; i < numElements; i += 1)
+        outer: for (int i = 2; i <= numElements; i += 1)
             for (int j = 1; j < i; j += 1) {
                 int low = numElements - j;
                 int high = numElements - i;
                 if (buildArray[low] > buildArray[high]) {
         // Swap compared values
+                    System.out.println("Yup, if statement matched");
                     int temp;
                     temp = buildArray[high];
                     buildArray[high] = buildArray[low];
@@ -49,15 +46,10 @@ public class NextBiggestNumber {
         // Create list of elements, stored in indeces above 'i'.
                     int[] ordArr;
                     ordArr = Arrays.copyOfRange( buildArray, (high + 1), numElements );
-//                    List<Integer> preOrdList = IntStream.of( ordArr ).boxed().collect( Collectors.toList() );
-//                    preOrdList.remove( oLength - i );
-//                    ordArr = preOrdList.stream().mapToInt( Integer::intValue ).toArray();
                     int oLength = ordArr.length;
 
                     // Call appropriate method to reorder portion of 'buildArray'.
-                    if (oLength == 1) {
-
-                    } else if (oLength == 2) {
+                   if (oLength == 2) {
                         wipe2( buildArray, ordArr );
                     } else if (oLength == 3) {
                         wipe3( buildArray, ordArr );
@@ -66,15 +58,13 @@ public class NextBiggestNumber {
                     } else if (oLength == 5) {
                         wipe5( buildArray, ordArr );
                     }
-                    break;
-                }
-                if (origBuild != buildArray){
-                    break;
+        // Once the match is found, end both loops by breaking outer loop
+                    break outer;
                 }
             }
 
 
-            // This rebuilds the appropriate array into an int to return.
+        // This rebuilds the appropriate array into an int to return.
             for (int k = 0; k < numElements; k++) {
                 rebuiltInt *= 10;
                 rebuiltInt += buildArray[k];
@@ -86,7 +76,7 @@ public class NextBiggestNumber {
         }
     }
 
-    private static int[] wipe2 ( int[] buildArr, int[] orderArr){
+    private static void wipe2 (int[] buildArr, int[] orderArr){
 
         int lnth = buildArr.length;
 
@@ -94,10 +84,9 @@ public class NextBiggestNumber {
         buildArr[lnth - 2] = orderArr[0];
         buildArr[lnth - 1] = orderArr[1];
 
-        return buildArr;
     }
 
-    private static int[] wipe3 ( int[] buildArr, int[] orderArr){
+    private static void wipe3 (int[] buildArr, int[] orderArr){
 
         int lnth = buildArr.length;
 
@@ -106,10 +95,9 @@ public class NextBiggestNumber {
         buildArr[lnth - 2] = orderArr[1];
         buildArr[lnth - 1] = orderArr[2];
 
-        return buildArr;
     }
 
-    public static int[] wipe4 (int[] buildArr, int[] orderArr){
+    public static void wipe4 (int[] buildArr, int[] orderArr){
 
         int lnth = buildArr.length;
 
@@ -119,10 +107,9 @@ public class NextBiggestNumber {
         buildArr[lnth - 2] = orderArr[2];
         buildArr[lnth - 1] = orderArr[3];
 
-        return buildArr;
     }
 
-    public static int[] wipe5 ( int[] buildArr, int[] orderArr){
+    public static void wipe5 (int[] buildArr, int[] orderArr){
 
         int lnth = buildArr.length;
 
@@ -133,6 +120,5 @@ public class NextBiggestNumber {
         buildArr[lnth - 2] = orderArr[3];
         buildArr[lnth - 1] = orderArr[4];
 
-        return buildArr;
     }
 }
